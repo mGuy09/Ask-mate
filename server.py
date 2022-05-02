4a984b74d445db8eb12cf8c8ca5dd6d25cae25c4
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 12:03:47 +0300

    Revert "[changes]  list-page.html"
    This reverts commit 68a424e35899e97ee0d3e57c47706ddda0b82900.

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 import data_manager
-import util
 app = Flask(__name__)
 
 '''
@@ -12,7 +11,6 @@ strs =  sorted(lista, key = lambda key: key['submission_time'])
 def list_page():
     question_data = data_manager.questions_data
     time = sorted(question_data, key = lambda i: i['submission_time'])
-
     return render_template("list-page.html", data=time)
 
 
