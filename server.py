3674127525eda3470d6396201f222ca5b8ac6134
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 13:51:06 +0300

    [fix] built-in shadowing

--- server.py
+++ server.py
@@ -16,28 +16,27 @@ strs =  sorted(lista, key = lambda key: key['submission_time'])
 @app.route("/list")
 def list_page():
     question_data = data_manager.get_data(questions)
-    time = sorted(question_data, key = lambda i: i['submission_time'])
+    time = sorted(question_data, key = lambda i: i['submission_time'], reverse=True)
     for i in time:
         if i['submission_time'] != format('%d/%m/%Y  %H:%M'):
             i['submission_time'] = util.convert_time(i['submission_time'])
     return render_template("list-page.html", data=time)
 
 
-
 @app.route('/question/<id>')
-def question(id):
+def question(question_id):
     question_data = data_manager.get_data(questions)
     answer_data = data_manager.get_data(answers)
     answer_list = []
     question_list = []
     for question in question_data:
-        if question['id'] == id:
+        if question['id'] == question_id:
             title = question['title']
             message = question['message']
             question_list.append(title)
             question_list.append(message)
             for answer in answer_data:
-                if id  == answer['question_id']:
+                if question_id == answer['question_id']:
                     answer_list.append(answer)
 
 
