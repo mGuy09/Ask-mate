106dc38cc19e61aedf5220d2cf5d2a3dfa4d6785
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 12:13:26 +0300

    [feat] datetime

--- server.py
+++ server.py
@@ -7,19 +7,14 @@ app = Flask(__name__)
 strs =  sorted(lista, key = lambda key: key['submission_time'])
 '''
 
+
 @app.route("/")
 @app.route("/list")
 def list_page():
     question_data = data_manager.questions_data
     time = sorted(question_data, key = lambda i: i['submission_time'])
-<<<<<<< HEAD
     for i in time:
-       i['submission_time'] = util.convert_time(i['submission_time'])
-
-
-=======
->>>>>>> 68a424e35899e97ee0d3e57c47706ddda0b82900
-
+        i['submission_time'] = util.convert_time(i['submission_time'])
     return render_template("list-page.html", data=time)
 
 
--- util.py
+++ util.py
@@ -3,5 +3,6 @@ import datetime
 
 
 def convert_time(time_in_millis):
-    dt = datetime.datetime.fromtimestamp(int(time_in_millis) / 1000.0)
+    dt = datetime.datetime.fromtimestamp(int(time_in_millis) / 1000.0,)
+    dt = dt.strftime('%d/%m/%Y  %H:%M')
     return dt
