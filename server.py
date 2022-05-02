93be1b2e75f99391e47e189388b950f5dddc2f9e
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 10:43:18 +0300

    [feat] data_manager functions in server.py

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def list_page():
-
-    return render_template("list-page.html")
+    question_data = data_manager.questions
+    return render_template("list-page.html", data=question_data)
 
 
 if __name__ == "__main__":
