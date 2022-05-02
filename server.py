d284e3633d45ab1a5bfc2282cc39c75c83b5489d
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-05-02 11:50:24 +0300

    feat datetime sorted

--- server.py
+++ server.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template, request, redirect
 import data_manager
 app = Flask(__name__)
 
-
+'''
+strs =  sorted(lista, key = lambda key: key['submission_time'])
+'''
 
 @app.route("/")
 @app.route("/list")
 def list_page():
     question_data = data_manager.questions_data
-    return render_template("list-page.html", data=question_data)
+    time = sorted(question_data, key = lambda i: i['submission_time'])
+    return render_template("list-page.html", data=time)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -4,5 +4,4 @@ import datetime
 
 def convert_time(time_in_millis):
     dt = datetime.datetime.fromtimestamp(time_in_millis / 1000.0)
-
     return dt
