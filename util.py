da9057a55a3517d432ce28610edb91fccadd7c67
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-05-02 11:41:56 +0300

    feat util.py

--- util.py
+++ util.py
@@ -2,7 +2,7 @@
 import datetime
 
 
-time_in_millis = 1596542285000
-dt = datetime.datetime.fromtimestamp(time_in_millis / 1000.0)
+def convert_time(time_in_millis):
+    dt = datetime.datetime.fromtimestamp(time_in_millis / 1000.0)
 
-print(dt)
+    return dt
