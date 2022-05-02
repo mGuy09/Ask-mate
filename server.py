af527ba707570bd0e26e7ea772ce2d9127ced879
GeorgeAnghel114 <georgeanghel114@gmail.com>
2022-05-02 10:30:17 +0300

    list-page.html

--- server.py
+++ server.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, redirect
 import connection
 app = Flask(__name__)
 
-question = util.read_question('question.csv')
-answer = util.read_question('answer.csv')
+question = connection.read_question('question.csv')
+answer = connection.read_question('answer.csv')
 
 
 @app.route("/")
 @app.route("/list")
 def list_page():
+
     return render_template("list-page.html")
 
 
