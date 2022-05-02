73fdb5fc906a9179fb25ee5d237898a3e0812b40
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 13:17:20 +0300

    [fix] date and time in table error

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,6 @@
-import os
-
 import connection
 
 
-questions = os.environ["QUESTION"]
-answers = os.environ["ANSWER"]
-
-questions_data = connection.read_question(questions)
-answers_data = connection.read_question(answers)
+def get_data(csv_file):
+    csv_data = connection.read_question(csv_file)
+    return csv_data
--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect
 import data_manager
 import util
+import os
+import datetime
 app = Flask(__name__)
 
+questions = os.environ["QUESTION"]
+answers = os.environ["ANSWER"]
 '''
 strs =  sorted(lista, key = lambda key: key['submission_time'])
 '''
@@ -11,10 +15,11 @@ strs =  sorted(lista, key = lambda key: key['submission_time'])
 @app.route("/")
 @app.route("/list")
 def list_page():
-    question_data = data_manager.questions_data
+    question_data = data_manager.get_data(questions)
     time = sorted(question_data, key = lambda i: i['submission_time'])
     for i in time:
-        i['submission_time'] = util.convert_time(i['submission_time'])
+        if i['submission_time'] != format('%d/%m/%Y  %H:%M'):
+            i['submission_time'] = util.convert_time(i['submission_time'])
     return render_template("list-page.html", data=time)
 
 
