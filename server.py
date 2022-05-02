f5323748859226b0bffc65f6c38d3719d1df1ec0
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 09:56:49 +0300

    [feat] added route to /list

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/list")
+def list_page():
+    return render_template("list-page.html")
 
 
 if __name__ == "__main__":
