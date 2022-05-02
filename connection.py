57322d6301d973ff9a33f83c607f2624a2a3d7cb
m_Guy09 <tudor.andreic19@gmail.com>
2022-05-02 15:34:09 +0300

    [feat] connection.py

--- connection.py
+++ connection.py
@@ -7,5 +7,14 @@ def read_question(data_csv):
         read_csv = csv.DictReader(file)
         for row in read_csv:
             list_of_samples.append(row)
+        return list_of_samples
 
-    return list_of_samples
+
+def get_new_id(data_csv):
+    return max([int(entry.get("id", 0)) for entry in read_question(data_csv)]) + 1
+
+
+def append_question(data_csv, data):
+    with open(data_csv, "a") as file:
+        writer = csv.DictWriter(file)
+        writer.writerow(data)
