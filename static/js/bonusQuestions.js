6832f6064e7871f8b8bbf14c5ae704009aea3431
m_Guy09 <tudor.andreic19@gmail.com>
2022-06-01 15:29:10 +0300

    fixed the merge problem sorry

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -25,18 +25,21 @@ function getSortedItems(items, sortField, sortDirection) {
 
 // you receive an array of objects which you must filter by all it's keys to have a value matching "filterValue"
 function getFilteredItems(items, filterValue) {
-    console.log(items)
-    console.log(filterValue)
+    // console.log(items)
+    // console.log(filterValue)
 
     // === SAMPLE CODE ===
     // if you have not changed the original html uncomment the code below to have an idea of the
     // effect this function has on the table
     //
-    for (let i=0; i<filterValue.length; i++) {
-        items.pop()
-    }
-
-    return items
+    let mylist = []
+    items.forEach(item => {
+        console.log(item)
+        if (item['Title'].includes(filterValue)){
+            mylist.push(item)
+        }
+    })
+    return mylist
 }
 
 function toggleTheme() {
